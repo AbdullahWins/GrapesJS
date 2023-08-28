35f2fa4bb4e32e40e5828735ed15732cce5ec79f
Abdullah Al MahMud <abdudevs@gmail.com>
2023-08-29 01:19:30 +0600

    updated the editor and added an element

---
+++ src/components/DragAndDrop.jsx
@@ -0,0 +1,7 @@
+const DragAndDrop = () => {
+    return (
+      <div>I can now be moved around!</div>
+    );
+};
+
+export default DragAndDrop;
--- src/components/ThisIsABox.jsx
+++ src/components/ThisIsABox.jsx
@@ -1,30 +1,73 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import grapesjs from 'grapesjs';
 import Card from '../elements/Card';
 import ReactDOMServer from 'react-dom/server';
 
-const ThisIsABox = () => {
+const GrapesJsEditor = () => {
   const editorRef = useRef(null);
+  const [editor, setEditor] = useState(null);
+  const [jsxCode, setJsxCode] = useState('');
 
-  //initializing the editor upon loading
+  const handleSave = () => {
+    if (editor) {
+      const canvasHTML = editor.getHtml(); // Get the canvas content as HTML
+
+      // Convert HTML to JSX
+      const jsxCode = htmlToJsx(canvasHTML);
+      setJsxCode(jsxCode);
+
+      // Trigger the download
+      downloadJsxFile(jsxCode);
+    }
+  };
+
+  // Convert HTML to JSX
+  const htmlToJsx = (html) => {
+    const jsx = html.replace(/class=/g, 'className='); // Convert class to className
+    return `<div>${jsx}</div>`;
+  };
+
+  // Trigger download of JSX file
+  const downloadJsxFile = (content) => {
+    const element = document.createElement('a');
+    const file = new Blob([content], { type: 'text/javascript' });
+    element.href = URL.createObjectURL(file);
+    element.download = 'generatedComponent.jsx';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
   useEffect(() => {
-    const editor = grapesjs.init({
+    const newEditor = grapesjs.init({
       container: editorRef.current,
-    },[]);
-
-    //converting the react component to raw html
-    const CardReactToHTML = ReactDOMServer.renderToString(<Card />);
-    //adding the component to the editor
-    editor.BlockManager.add('custom-element', {
-      label: 'Card',
-      content: CardReactToHTML, // Use the converted HTML here
     });
+
+    setEditor(newEditor);
+
+    const CardReactToHTML = ReactDOMServer.renderToString(<Card />);
+    newEditor.BlockManager.add('custom-element', {
+      label: 'Card',
+      content: CardReactToHTML,
+    });
+
     return () => {
-      editor.destroy();
+      newEditor.destroy();
     };
   }, []);
 
-  return <div ref={editorRef}></div>;
+  return (
+    <div>
+      <div ref={editorRef}></div>
+      <button onClick={handleSave}>Generate and Download Component</button>
+      {jsxCode && (
+        <div>
+          <h3>Generated JSX:</h3>
+          <pre>{jsxCode}</pre>
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default ThisIsABox;
+export default GrapesJsEditor;
