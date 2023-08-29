4e984c11eea243e6ffa775a56725c00237034be8
Abdullah Al MahMud <abdudevs@gmail.com>
2023-08-29 13:42:52 +0600

    todo branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import GrapesJsEditor from "./pages/GrapesJsEditor";
-// import ToDo from "./pages/ToDo";
+// import GrapesJsEditor from "./pages/GrapesJsEditor";
+import ToDo from "./pages/ToDo";
 
 function App() {
   return (
     <div className=" bg-gray-200 w-full min-h-screen">
-      <GrapesJsEditor></GrapesJsEditor>
+      {/* <GrapesJsEditor></GrapesJsEditor> */}
       <div className="flex items-center justify-center gap-4">
-        {/* <ToDo></ToDo> */}
+        <ToDo></ToDo>
       </div>
     </div>
   );
